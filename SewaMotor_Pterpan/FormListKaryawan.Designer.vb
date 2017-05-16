3373e4d50409b7e411ba9b0141c1d250d666fff5
GalangSusetyawan <galangs123@gmail.com>
2017-05-16 16:48:22 +0700

    List Karyawan

---
+++ SewaMotor_Pterpan/FormListKaryawan.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormListKaryawan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.groupDaftarPelanggan = New System.Windows.Forms.GroupBox()
+        Me.lblCari = New System.Windows.Forms.Label()
+        Me.txtCari = New System.Windows.Forms.TextBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUbah = New System.Windows.Forms.Button()
+        Me.dgvKaryawan = New System.Windows.Forms.DataGridView()
+        Me.SewaMotorDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.SewaMotorDataSet = New SewaMotor_Pterpan.SewaMotorDataSet()
+        Me.loginToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.homeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.katalogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.groupDaftarPelanggan.SuspendLayout()
+        CType(Me.dgvKaryawan, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SewaMotorDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SewaMotorDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.menuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupDaftarPelanggan
+        '
+        Me.groupDaftarPelanggan.Controls.Add(Me.btnTambah)
+        Me.groupDaftarPelanggan.Controls.Add(Me.lblCari)
+        Me.groupDaftarPelanggan.Controls.Add(Me.txtCari)
+        Me.groupDaftarPelanggan.Controls.Add(Me.btnDelete)
+        Me.groupDaftarPelanggan.Controls.Add(Me.btnUbah)
+        Me.groupDaftarPelanggan.Controls.Add(Me.dgvKaryawan)
+        Me.groupDaftarPelanggan.Location = New System.Drawing.Point(12, 27)
+        Me.groupDaftarPelanggan.Name = "groupDaftarPelanggan"
+        Me.groupDaftarPelanggan.Size = New System.Drawing.Size(684, 351)
+        Me.groupDaftarPelanggan.TabIndex = 31
+        Me.groupDaftarPelanggan.TabStop = False
+        Me.groupDaftarPelanggan.Text = "Daftar Karyawan"
+        '
+        'lblCari
+        '
+        Me.lblCari.AutoSize = True
+        Me.lblCari.Location = New System.Drawing.Point(275, 32)
+        Me.lblCari.Name = "lblCari"
+        Me.lblCari.Size = New System.Drawing.Size(81, 13)
+        Me.lblCari.TabIndex = 19
+        Me.lblCari.Text = "Cari Karyawan: "
+        '
+        'txtCari
+        '
+        Me.txtCari.Location = New System.Drawing.Point(366, 29)
+        Me.txtCari.Name = "txtCari"
+        Me.txtCari.Size = New System.Drawing.Size(306, 20)
+        Me.txtCari.TabIndex = 14
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(179, 322)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 17
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUbah
+        '
+        Me.btnUbah.Location = New System.Drawing.Point(98, 322)
+        Me.btnUbah.Name = "btnUbah"
+        Me.btnUbah.Size = New System.Drawing.Size(75, 23)
+        Me.btnUbah.TabIndex = 18
+        Me.btnUbah.Text = "Ubah"
+        Me.btnUbah.UseVisualStyleBackColor = True
+        '
+        'dgvKaryawan
+        '
+        Me.dgvKaryawan.AllowUserToAddRows = False
+        Me.dgvKaryawan.AllowUserToDeleteRows = False
+        Me.dgvKaryawan.AllowUserToOrderColumns = True
+        Me.dgvKaryawan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvKaryawan.Location = New System.Drawing.Point(11, 55)
+        Me.dgvKaryawan.Name = "dgvKaryawan"
+        Me.dgvKaryawan.ReadOnly = True
+        Me.dgvKaryawan.Size = New System.Drawing.Size(661, 261)
+        Me.dgvKaryawan.TabIndex = 15
+        '
+        'SewaMotorDataSetBindingSource
+        '
+        Me.SewaMotorDataSetBindingSource.DataSource = Me.SewaMotorDataSet
+        Me.SewaMotorDataSetBindingSource.Position = 0
+        '
+        'SewaMotorDataSet
+        '
+        Me.SewaMotorDataSet.DataSetName = "SewaMotorDataSet"
+        Me.SewaMotorDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'loginToolStripMenuItem
+        '
+        Me.loginToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.loginToolStripMenuItem.Name = "loginToolStripMenuItem"
+        Me.loginToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.loginToolStripMenuItem.Text = "Logout"
+        '
+        'menuStrip1
+        '
+        Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.homeToolStripMenuItem, Me.katalogToolStripMenuItem, Me.loginToolStripMenuItem})
+        Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.menuStrip1.Name = "menuStrip1"
+        Me.menuStrip1.Size = New System.Drawing.Size(629, 24)
+        Me.menuStrip1.TabIndex = 30
+        Me.menuStrip1.Text = "menuStrip1"
+        '
+        'homeToolStripMenuItem
+        '
+        Me.homeToolStripMenuItem.Name = "homeToolStripMenuItem"
+        Me.homeToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.homeToolStripMenuItem.Text = "Home"
+        '
+        'katalogToolStripMenuItem
+        '
+        Me.katalogToolStripMenuItem.Name = "katalogToolStripMenuItem"
+        Me.katalogToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.katalogToolStripMenuItem.Text = "Katalog"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(266, 100)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(0, 13)
+        Me.label2.TabIndex = 29
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(11, 322)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(75, 23)
+        Me.btnTambah.TabIndex = 20
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'FormListKaryawan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(629, 411)
+        Me.Controls.Add(Me.groupDaftarPelanggan)
+        Me.Controls.Add(Me.menuStrip1)
+        Me.Controls.Add(Me.label2)
+        Me.Name = "FormListKaryawan"
+        Me.Text = "FormListKaryawan"
+        Me.groupDaftarPelanggan.ResumeLayout(False)
+        Me.groupDaftarPelanggan.PerformLayout()
+        CType(Me.dgvKaryawan, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SewaMotorDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SewaMotorDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.menuStrip1.ResumeLayout(False)
+        Me.menuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents groupDaftarPelanggan As GroupBox
+    Private WithEvents lblCari As Label
+    Friend WithEvents txtCari As TextBox
+    Private WithEvents btnDelete As Button
+    Private WithEvents btnUbah As Button
+    Friend WithEvents dgvKaryawan As DataGridView
+    Friend WithEvents SewaMotorDataSetBindingSource As BindingSource
+    Friend WithEvents SewaMotorDataSet As SewaMotorDataSet
+    Private WithEvents loginToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents menuStrip1 As MenuStrip
+    Private WithEvents homeToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents katalogToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents label2 As Label
+    Friend WithEvents btnTambah As Button
+End Class
---
+++ SewaMotor_Pterpan/FormListKaryawan.vb
@@ -0,0 +1,74 @@
+﻿Public Class FormListKaryawan
+    Private karyawan As New Tabel("Karyawan")
+    Private idKaryawan As Integer
+
+    '/////LOAD/////
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+    Private Sub FormListKaryawan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgvKaryawan.DataSource = karyawan.getBS()
+        If dgvKaryawan.Columns.Count > 0 Then
+            dgvKaryawan.Columns(4).Visible = False
+            dgvKaryawan.Columns(5).Visible = False
+        End If
+    End Sub
+
+    '/////BUTTON/////
+    Private Sub btnUbah_Click(sender As Object, e As EventArgs) Handles btnUbah.Click
+        Edit()
+    End Sub
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Delete()
+    End Sub
+
+    '/////TextBox/////
+    Private Sub txtCari_TextChanged(sender As Object, e As EventArgs) Handles txtCari.TextChanged
+        Find()
+    End Sub
+
+    '/////DGV/////
+    Private Sub dgvKaryawan_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvKaryawan.CellContentClick
+        idKaryawan = dgvKaryawan.Item(0, dgvKaryawan.CurrentRow.Index).Value
+    End Sub
+    Private Sub dgvKaryawan_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvKaryawan.CellContentDoubleClick
+        Edit()
+    End Sub
+    Private Sub dgvKaryawan_KeyDown(sender As Object, e As KeyEventArgs) Handles dgvKaryawan.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Edit()
+        ElseIf e.KeyCode = Keys.Delete Then
+            Delete()
+        End If
+    End Sub
+
+
+    '/////FUNCTION/////
+    Private Sub Find()
+        If txtCari.Text.Length <= 20 Then
+            If txtCari.Text = "" Then
+                karyawan.getBS.Filter = ""
+                dgvKaryawan.DataSource = karyawan.getBS()
+            Else
+                karyawan.getBS.Filter = "nama Like '" & txtCari.Text & "%'"
+            End If
+        End If
+
+    End Sub
+    Private Sub Edit()
+        idKaryawan = dgvKaryawan.Item(0, dgvKaryawan.CurrentRow.Index).Value
+        'Dim editkaryawan As New FormEditkaryawan(idKaryawan)
+        'editkaryawan.ShowDialog()
+    End Sub
+    Private Sub Delete()
+        idKaryawan = dgvKaryawan.Item(0, dgvKaryawan.CurrentRow.Index).Value
+        Dim namakaryawan As String = dgvKaryawan.Item(2, dgvKaryawan.CurrentRow.Index).Value
+        Dim result As Integer = MessageBox.Show("Apakah anda yakin ingin menghapus karyawan " & namakaryawan, "Peringatan", MessageBoxButtons.YesNo)
+        If result = DialogResult.No Then
+            Return
+        ElseIf result = DialogResult.Yes Then
+            karyawan.isiDataTable("DELETE FROM karyawan WHERE id=" & idKaryawan, "karyawan " & namakaryawan & " berhasil dihapus!")
+        End If
+    End Sub
+
+End Class

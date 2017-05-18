fbe9d682a7acefad40c9c3284bd5a0872217b32f
GalangSusetyawan <galangs123@gmail.com>
2017-05-18 13:48:45 +0700

    coba

---
+++ SewaMotor_Pterpan/Nama.vb
@@ -0,0 +1,11 @@
+﻿Public Class Nama
+    Private _username As String
+    Public Property username() As String
+        Get
+            Return _username
+        End Get
+        Set(ByVal value As String)
+            _username = value
+        End Set
+    End Property
+End Class

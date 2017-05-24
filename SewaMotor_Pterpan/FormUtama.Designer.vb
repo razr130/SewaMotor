46ab36c6c5eec6e95b17811cb2b17f6ca260fe92
JV Fernando <jovfernando@yahoo.com>
2017-05-24 19:30:26 +0700

    gwu

--- SewaMotor_Pterpan/FormUtama.Designer.vb
+++ SewaMotor_Pterpan/FormUtama.Designer.vb
@@ -203,10 +203,10 @@ Partial Class FormUtama
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 134)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 137)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(113, 24)
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
         Me.Label2.TabIndex = 13
         Me.Label2.Text = "Pilih Motor : "
         '

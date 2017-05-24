84589cf54e22fbaf4facb30615edc0571d7c3d7e
GalangSusetyawan <galangs123@gmail.com>
2017-05-24 13:43:15 +0700

    Testing ReportViewer

--- SewaMotor_Pterpan/FormLaporan.Designer.vb
+++ SewaMotor_Pterpan/FormLaporan.Designer.vb
@@ -22,16 +22,27 @@ Partial Class FormLaporan
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Location = New System.Drawing.Point(74, 52)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(396, 246)
+        Me.ReportViewer1.TabIndex = 0
         '
         'FormLaporan
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(449, 331)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "FormLaporan"
         Me.Text = "FormLaporan"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
 End Class
--- SewaMotor_Pterpan/FormLaporan.vb
+++ SewaMotor_Pterpan/FormLaporan.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormLaporan
+    Private Sub FormLaporan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        Me.ReportViewer1.RefreshReport
+    End Sub
 End Class

070b39cbb505a2aee3823dc5513266367c194cf3
GalangSusetyawan <galangs123@gmail.com>
2017-05-26 23:56:36 +0700

    ~

---
+++ SewaMotor_Pterpan/FormCheckoutPengembalian.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCheckoutPengembalian
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.homeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.logOutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.AutoSize = True
+        ReportDataSource2.Name = "InvoiceSewaDataSet"
+        ReportDataSource2.Value = Nothing
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "SewaMotor_Pterpan.InvoiceSewa.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 27)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ShowBackButton = False
+        Me.ReportViewer1.ShowExportButton = False
+        Me.ReportViewer1.ShowFindControls = False
+        Me.ReportViewer1.ShowPageNavigationControls = False
+        Me.ReportViewer1.ShowPrintButton = False
+        Me.ReportViewer1.ShowRefreshButton = False
+        Me.ReportViewer1.ShowStopButton = False
+        Me.ReportViewer1.ShowZoomControl = False
+        Me.ReportViewer1.Size = New System.Drawing.Size(802, 468)
+        Me.ReportViewer1.TabIndex = 39
+        '
+        'menuStrip1
+        '
+        Me.menuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.homeToolStripMenuItem, Me.logOutToolStripMenuItem})
+        Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.menuStrip1.Name = "menuStrip1"
+        Me.menuStrip1.Size = New System.Drawing.Size(826, 24)
+        Me.menuStrip1.TabIndex = 40
+        Me.menuStrip1.Text = "menuStrip1"
+        '
+        'homeToolStripMenuItem
+        '
+        Me.homeToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.homeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.homeToolStripMenuItem.Name = "homeToolStripMenuItem"
+        Me.homeToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.homeToolStripMenuItem.Text = "Home"
+        '
+        'logOutToolStripMenuItem
+        '
+        Me.logOutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.logOutToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.logOutToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem"
+        Me.logOutToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.logOutToolStripMenuItem.Text = "Logout"
+        '
+        'FormCheckoutPengembalian
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(826, 506)
+        Me.Controls.Add(Me.menuStrip1)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "FormCheckoutPengembalian"
+        Me.Text = "FormCheckoutPengembalian"
+        Me.menuStrip1.ResumeLayout(False)
+        Me.menuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Private WithEvents menuStrip1 As MenuStrip
+    Private WithEvents homeToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents logOutToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ SewaMotor_Pterpan/GlobalVariables.vb
@@ -0,0 +1,5 @@
+﻿Public Class GlobalVariables
+    Public Shared UserName As String
+    Public Shared Role As Integer
+    Public Shared NoNota As Integer
+End Class

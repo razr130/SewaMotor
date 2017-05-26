ea405ca9cceae62b5ad3ce3bedc8b69fe77c0df5
GalangSusetyawan <galangs123@gmail.com>
2017-05-27 00:04:29 +0700

    123

--- SewaMotor_Pterpan/FormCheckout.Designer.vb
+++ SewaMotor_Pterpan/FormCheckout.Designer.vb
@@ -23,26 +23,43 @@ Partial Class FormCheckout
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ViewNotaPeminjamanBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.InvoiceSewaDataSet = New SewaMotor_Pterpan.InvoiceSewaDataSet()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.ViewNotaPeminjamanTableAdapter = New SewaMotor_Pterpan.InvoiceSewaDataSetTableAdapters.ViewNotaPeminjamanTableAdapter()
         Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.homeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.katalogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.loginToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.InvoiceSewaDataSet = New SewaMotor_Pterpan.InvoiceSewaDataSet()
-        Me.ViewNotaPeminjamanBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.ViewNotaPeminjamanTableAdapter = New SewaMotor_Pterpan.InvoiceSewaDataSetTableAdapters.ViewNotaPeminjamanTableAdapter()
-        Me.menuStrip1.SuspendLayout()
-        CType(Me.InvoiceSewaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.logOutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListPelangganToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListKaryawanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListOrderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LaporanPenyewaanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LaporanStatusMotorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LaporanKeterlambatanMotorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.ViewNotaPeminjamanBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.InvoiceSewaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.menuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'ViewNotaPeminjamanBindingSource
+        '
+        Me.ViewNotaPeminjamanBindingSource.DataMember = "ViewNotaPeminjaman"
+        Me.ViewNotaPeminjamanBindingSource.DataSource = Me.InvoiceSewaDataSet
+        '
+        'InvoiceSewaDataSet
+        '
+        Me.InvoiceSewaDataSet.DataSetName = "InvoiceSewaDataSet"
+        Me.InvoiceSewaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'ReportViewer1
         '
         Me.ReportViewer1.AutoSize = True
-        ReportDataSource1.Name = "InvoiceSewaDataSet"
-        ReportDataSource1.Value = Me.ViewNotaPeminjamanBindingSource
-        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        ReportDataSource2.Name = "InvoiceSewaDataSet"
+        ReportDataSource2.Value = Me.ViewNotaPeminjamanBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "SewaMotor_Pterpan.InvoiceSewa.rdlc"
         Me.ReportViewer1.Location = New System.Drawing.Point(10, 27)
         Me.ReportViewer1.Name = "ReportViewer1"
@@ -57,9 +74,14 @@ Partial Class FormCheckout
         Me.ReportViewer1.Size = New System.Drawing.Size(711, 468)
         Me.ReportViewer1.TabIndex = 38
         '
+        'ViewNotaPeminjamanTableAdapter
+        '
+        Me.ViewNotaPeminjamanTableAdapter.ClearBeforeFill = True
+        '
         'menuStrip1
         '
-        Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.homeToolStripMenuItem, Me.katalogToolStripMenuItem, Me.loginToolStripMenuItem})
+        Me.menuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.homeToolStripMenuItem, Me.logOutToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.ReportToolStripMenuItem})
         Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.menuStrip1.Name = "menuStrip1"
         Me.menuStrip1.Size = New System.Drawing.Size(788, 24)
@@ -68,36 +90,74 @@ Partial Class FormCheckout
         '
         'homeToolStripMenuItem
         '
+        Me.homeToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.homeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.homeToolStripMenuItem.Name = "homeToolStripMenuItem"
-        Me.homeToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.homeToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
         Me.homeToolStripMenuItem.Text = "Home"
         '
-        'katalogToolStripMenuItem
+        'logOutToolStripMenuItem
         '
-        Me.katalogToolStripMenuItem.Name = "katalogToolStripMenuItem"
-        Me.katalogToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
-        Me.katalogToolStripMenuItem.Text = "Katalog"
+        Me.logOutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.logOutToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.logOutToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem"
+        Me.logOutToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.logOutToolStripMenuItem.Text = "Logout"
         '
-        'loginToolStripMenuItem
+        'ToolsToolStripMenuItem
         '
-        Me.loginToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
-        Me.loginToolStripMenuItem.Name = "loginToolStripMenuItem"
-        Me.loginToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
-        Me.loginToolStripMenuItem.Text = "Logout"
+        Me.ToolsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ListPelangganToolStripMenuItem, Me.ListKaryawanToolStripMenuItem, Me.ListOrderToolStripMenuItem})
+        Me.ToolsToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ToolsToolStripMenuItem.Name = "ToolsToolStripMenuItem"
+        Me.ToolsToolStripMenuItem.Size = New System.Drawing.Size(47, 20)
+        Me.ToolsToolStripMenuItem.Text = "Tools"
         '
-        'InvoiceSewaDataSet
+        'ListPelangganToolStripMenuItem
         '
-        Me.InvoiceSewaDataSet.DataSetName = "InvoiceSewaDataSet"
-        Me.InvoiceSewaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        Me.ListPelangganToolStripMenuItem.Name = "ListPelangganToolStripMenuItem"
+        Me.ListPelangganToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ListPelangganToolStripMenuItem.Text = "List Pelanggan"
         '
-        'ViewNotaPeminjamanBindingSource
+        'ListKaryawanToolStripMenuItem
         '
-        Me.ViewNotaPeminjamanBindingSource.DataMember = "ViewNotaPeminjaman"
-        Me.ViewNotaPeminjamanBindingSource.DataSource = Me.InvoiceSewaDataSet
+        Me.ListKaryawanToolStripMenuItem.Name = "ListKaryawanToolStripMenuItem"
+        Me.ListKaryawanToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ListKaryawanToolStripMenuItem.Text = "List Karyawan"
         '
-        'ViewNotaPeminjamanTableAdapter
+        'ListOrderToolStripMenuItem
         '
-        Me.ViewNotaPeminjamanTableAdapter.ClearBeforeFill = True
+        Me.ListOrderToolStripMenuItem.Name = "ListOrderToolStripMenuItem"
+        Me.ListOrderToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ListOrderToolStripMenuItem.Text = "List Order"
+        '
+        'ReportToolStripMenuItem
+        '
+        Me.ReportToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LaporanPenyewaanToolStripMenuItem, Me.LaporanStatusMotorToolStripMenuItem, Me.LaporanKeterlambatanMotorToolStripMenuItem})
+        Me.ReportToolStripMenuItem.Font = New System.Drawing.Font("Franklin Gothic Demi", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReportToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ReportToolStripMenuItem.Name = "ReportToolStripMenuItem"
+        Me.ReportToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.ReportToolStripMenuItem.Text = "Report"
+        '
+        'LaporanPenyewaanToolStripMenuItem
+        '
+        Me.LaporanPenyewaanToolStripMenuItem.Name = "LaporanPenyewaanToolStripMenuItem"
+        Me.LaporanPenyewaanToolStripMenuItem.Size = New System.Drawing.Size(235, 22)
+        Me.LaporanPenyewaanToolStripMenuItem.Text = "Laporan Penyewaan"
+        '
+        'LaporanStatusMotorToolStripMenuItem
+        '
+        Me.LaporanStatusMotorToolStripMenuItem.Name = "LaporanStatusMotorToolStripMenuItem"
+        Me.LaporanStatusMotorToolStripMenuItem.Size = New System.Drawing.Size(235, 22)
+        Me.LaporanStatusMotorToolStripMenuItem.Text = "Laporan Status Motor"
+        '
+        'LaporanKeterlambatanMotorToolStripMenuItem
+        '
+        Me.LaporanKeterlambatanMotorToolStripMenuItem.Name = "LaporanKeterlambatanMotorToolStripMenuItem"
+        Me.LaporanKeterlambatanMotorToolStripMenuItem.Size = New System.Drawing.Size(235, 22)
+        Me.LaporanKeterlambatanMotorToolStripMenuItem.Text = "Laporan Keterlambatan Motor"
         '
         'FormCheckout
         '
@@ -105,26 +165,33 @@ Partial Class FormCheckout
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(788, 507)
-        Me.Controls.Add(Me.ReportViewer1)
         Me.Controls.Add(Me.menuStrip1)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "FormCheckout"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FormCheckout"
+        CType(Me.ViewNotaPeminjamanBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.InvoiceSewaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.menuStrip1.ResumeLayout(False)
         Me.menuStrip1.PerformLayout()
-        CType(Me.InvoiceSewaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ViewNotaPeminjamanBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
-    Friend WithEvents menuStrip1 As MenuStrip
-    Private WithEvents homeToolStripMenuItem As ToolStripMenuItem
-    Private WithEvents katalogToolStripMenuItem As ToolStripMenuItem
-    Private WithEvents loginToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ViewNotaPeminjamanBindingSource As BindingSource
     Friend WithEvents InvoiceSewaDataSet As InvoiceSewaDataSet
     Friend WithEvents ViewNotaPeminjamanTableAdapter As InvoiceSewaDataSetTableAdapters.ViewNotaPeminjamanTableAdapter
+    Private WithEvents menuStrip1 As MenuStrip
+    Private WithEvents homeToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents logOutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListPelangganToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListKaryawanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListOrderToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LaporanPenyewaanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LaporanStatusMotorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LaporanKeterlambatanMotorToolStripMenuItem As ToolStripMenuItem
 End Class
